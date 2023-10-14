637e0f4848cdc88231c6d6f8144f1c519f2cbf4e
jasonlkh-wa <50670119+jasonlkh-wa@users.noreply.github.com>
2023-10-14 22:17:51 +0800

    Fix the setup.sh to properly configure .env file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 parent_dir=$(dirname "$(readlink -f "$0")")
 
 # create the function list csv
-csv_path="$parent_dir/resources/cmd2.csv"
+csv_path="$parent_dir/resources/cmd.csv"
 echo "cmd,file,description" >"$csv_path"
 
 # create the shell script path
@@ -10,14 +10,14 @@ mkdir -p "$(dirname "$shell_script_path")"
 touch "$shell_script_path"
 
 # rename example.env to .env
-env_path="$parent_dir/2.env"
-mv "$parent_dir/example.env" "$env_path"
+env_path="$parent_dir/.env"
+cp "$parent_dir/example.env" "$env_path"
 
 # replace the configuration in .env
-csv_line_number=2
+csv_line_number=3
 new_csv_source="CSV_SOURCE=\"$csv_path\""
 sed -i '' "${csv_line_number}s?.*?${new_csv_source}?" "$env_path"
 
-shell_script_line_number=1
+shell_script_line_number=2
 new_shell_script_path="SHELL_SCRIPT_PATH=\"$shell_script_path\""
 sed -i '' "${shell_script_line_number}s?.*?${new_shell_script_path}?" "$env_path"
